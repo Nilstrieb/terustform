b5a28e4e94f8637323b32fbd9bab82bb740511c6
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2024-04-15 20:42:22 +0200

    init corsschool

---
+++ terraform-provider-corsschool/test/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    corsschool = {
+        source = "github.com/Nilstrieb/corsschool"
+    }
+  }
+}
+
+provider "corsschool" {}
+
+//resource "terustform_hello" "test1" {}
+
+data "corsschool_kitty" "kitty" {
+  name = "aa mykitten"
+}
+data "corsschool_kitty" "hellyes" {
+  name = "aa a cute kitty"
+}
+output "cat1" {
+  value = data.corsschool_kitty.kitty.meow
+}
+output "cat2" {
+  value = data.corsschool_kitty.hellyes.meow
+}
